23d2a4d202289233687c5926aa9555248cd9e307
faijul-ast <faijul.madhi@arsenaltech.com>
2026-06-15 17:48:29 +0530

    build: upgrade dependencies and webpack config for Nova 5 migration
    - Update `package.json` to include `@inertiajs/vue3` and latest Vue 3.5 / Webpack versions
    - Move `vue` to devDependencies and remove unused `form-backend-validation`
    - Remove hardcoded `laravel-nova` aliases in `webpack.mix.js` and `nova.mix.js`
    - Set `laravel-nova` as a Webpack external in `nova.mix.js` to rely on the global `LaravelNova` object

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,14 +7,4 @@ mix
     .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
     .sass('resources/sass/field.scss', 'css')
-    .nova('pdmfc/nova-inline-text')
-    .alias({
-        '@': 'vendor/laravel/nova/resources/js/',
-    })
-    .webpackConfig({
-        resolve: {
-            alias: {
-                'laravel-nova': path.resolve(__dirname, './node_modules/laravel-nova/dist/index.js'),
-            },
-        },
-    });
+    .nova('pdmfc/nova-inline-text');
